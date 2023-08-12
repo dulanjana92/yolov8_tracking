1a5f742a1928b94c38926d23a5998d614c4a12a7
Dulanjana Gamage <dulanjana92@hotmail.com>
2023-08-12 13:10:11 +0100

    Test

--- tracker/detectors/__init__.py
+++ tracker/detectors/__init__.py
@@ -1,5 +1,7 @@
 # Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
+from boxmot.utils.checks import TestRequirements
 
+tr = TestRequirements()
 def get_yolo_inferer():    
     # ultralytics already installed when running track.py
     from .yolov8 import Yolov8Strategy

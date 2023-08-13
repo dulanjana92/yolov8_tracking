474c3f2d080891a072d76ad4af325febc5c18daa
Dulanjana Gamage <dulanjana92@hotmail.com>
2023-08-13 10:28:44 +0100

    test

--- tracker/utils.py
+++ tracker/utils.py
@@ -10,7 +10,7 @@ def write_mot_results(txt_path, results, frame_idx):
     frame_idx = torch.full((1, 1), frame_idx + 1)
     frame_idx = frame_idx.repeat(nr_dets, 1)
     dont_care = torch.full((nr_dets, 1), -1)
-    print(results)
+    #print(results)
     print(f"box count: {nr_dets}")
     if nr_dets > 0:
         mot = torch.cat([
